a349460bddae59faf06971ce61d616a998cfcc83
enzo <enzogabrielbustillo@gmail.com>
2025-05-20 19:29:12 -0400

    get it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inventario_medizin/config/router/app_router.dart';
+import 'package:inventario_medizin/config/services/service_locator.dart';
 import 'package:inventario_medizin/config/theme/app_theme.dart';
 
-
+Future<void> main() async {
+  setupLocator();
+  runApp(const ProviderScope(child: MainApp()));
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
